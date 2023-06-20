858827d0ed4200affb48656bf033bc7007f9a2f0
Z Fang <z.fang0512@gmail.com>
2023-06-20 19:07:26 +0200

    fix some bugs of environments

--- environments/simple_factory.py
+++ environments/simple_factory.py
@@ -89,7 +89,9 @@ class SimpleFactoryGymEnv(gym.Env):
         self.no_products_time = 0
 
     def _get_obs(self):
-        return np.array([self.factory.resources.level, self.factory.resources_c1.level, self.factory.resources_c2.level], 
+        return np.array([self.factory.resources.level/self.factory.resources.capacity, 
+                         self.factory.resources_c1.level/self.factory.resources_c1.capacity, 
+                         self.factory.resources_c2.level/self.factory.resources_c2.capacity], 
                         dtype=np.int32)
     
     def _get_info(self):
@@ -123,8 +125,8 @@ class SimpleFactoryGymEnv(gym.Env):
         
         current = self.simpy_env.now 
         current_products = self.factory.products.level
-        observation = self._get_obs()
-        info = self._get_info()
+        #observation = self._get_obs()
+        #info = self._get_info()
 
         self.simpy_env.process(resource_schedule(self.simpy_env, self.factory, action))
         self.simpy_env.process(self.produce_c1_gen)
@@ -132,6 +134,9 @@ class SimpleFactoryGymEnv(gym.Env):
         self.simpy_env.process(self.assemble_gen)
 
         self.simpy_env.run(until=current+self.step_time)
+        
+        observation = self._get_obs()
+        info = self._get_info()
 
         reward = self.factory.products.level - current_products    
 
